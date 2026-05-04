3e5e34d90d4de52ea5a06e033b1daf342c6bdb78
Nick-Lemy <n.kayiranga@alustudent.com>
2026-05-04 11:32:34 +0200

    fix: implement smooth scrolling and fade-in animation for page transitions

--- frontend/src/pages/RootLayer.tsx
+++ frontend/src/pages/RootLayer.tsx
@@ -1,16 +1,22 @@
-import { Outlet } from "react-router"
+import { useEffect } from "react"
+import { Outlet, useLocation } from "react-router"
 import MainNavigation from "../components/MainNavigation"
 import Footer from "../components/Footer"
 
-
-
-
 const RootLayer = () => {
+  const location = useLocation()
+
+  useEffect(() => {
+    window.scrollTo({ top: 0, behavior: "instant" as ScrollBehavior })
+  }, [location.pathname])
+
   return (
     <>
-    <MainNavigation/>
-    <Outlet/>
-    <Footer/>
+      <MainNavigation />
+      <main key={location.pathname} className="page-fade">
+        <Outlet />
+      </main>
+      <Footer />
     </>
   )
 }
