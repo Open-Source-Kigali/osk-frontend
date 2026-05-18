572da83ae9493fa32261ba8e86d283c3253ed4fb
HIRWA KAYIHURA Kevin <hirwakayihurakevin@gmail.com>
2026-05-18 22:09:50 +0200

    Fixed code of conduct to point to google docs link

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -167,9 +167,12 @@ const Footer = () => {
             <NavLink to="/privacy" className="hover:text-gray-400 transition">
               Privacy Policy
             </NavLink>
-            <NavLink to="/conduct" className="hover:text-gray-400 transition">
+            <a
+              href="https://docs.google.com/document/d/1K_bIZT09p-8IiPpg1v3RsMD2dZmUzRFWlMp8tfOavcE/edit?usp=sharing"
+              className="hover:text-gray-400 transition"
+            >
               Code of Conduct
-            </NavLink>
+            </a>
             <NavLink to="/charter" className="hover:text-gray-400 transition">
               Community Charter
             </NavLink>
