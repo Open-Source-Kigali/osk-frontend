b3521096f18d063fc5c6d766b36b83cdd13c182d
“Mudor” <mudorechristian@gmail.com>
2026-05-18 17:03:32 +0200

    Fixed Primmary button to open at new branch

--- src/components/UI/PrimaryButton.tsx
+++ src/components/UI/PrimaryButton.tsx
@@ -15,8 +15,8 @@ const PrimaryButton = ({
   return (
     <a
       href={to}
-      target="_blank"
       rel="noopener noreferrer"
+      target="_blank"
       className={`flex items-center justify-center gap-2 text-sm sm:text-base px-5 py-2.5 md:px-7 md:py-3.5 bg-primary-colour hover:bg-brand-500 hover:scale-95 text-white font-semibold rounded-full transition ${className}`}
     >
       {children}
