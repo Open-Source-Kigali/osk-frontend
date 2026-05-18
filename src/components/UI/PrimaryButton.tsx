ca36632c6802c63870b943c9f6efb3cb04918d5f
Victoire Ansima <ansimavicky@gmail.com>
2026-05-18 16:58:16 +0200

    fix : fixed the primary button to open in a new tab

--- src/components/UI/PrimaryButton.tsx
+++ src/components/UI/PrimaryButton.tsx
@@ -8,18 +8,17 @@ type PrimaryButtonProps = {
   className?: string;
 };
 
-const PrimaryButton = ({
-  to,
-  children,
-  className = "",
-}: PrimaryButtonProps) => {
+const PrimaryButton = ({ children, className, to }: PrimaryButtonProps) => {
   return (
-    <NavLink
-      to={to}
+    <a
+      className="cursor-pointer"
+      target="_blank"
+      rel="noopener noreferrer"
+      href={to}
       className={`flex items-center justify-center gap-2 text-sm sm:text-base px-5 py-2.5 md:px-7 md:py-3.5 bg-primary-colour hover:bg-brand-500 hover:scale-95 text-white font-semibold rounded-full transition ${className}`}
     >
       {children}
-    </NavLink>
+    </a>
   );
 };
 
