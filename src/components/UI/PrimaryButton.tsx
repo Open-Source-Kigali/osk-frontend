9f2737c2cb0e975d38d8a162c6eff78655227d26
MukamaBruce <mukamabruce40@gmail.com>
2026-05-18 16:47:42 -0700

    fix: add newTab support to PrimaryButton

--- src/components/UI/PrimaryButton.tsx
+++ src/components/UI/PrimaryButton.tsx
@@ -6,16 +6,20 @@ type PrimaryButtonProps = {
   children: React.ReactNode;
   icon?: boolean;
   className?: string;
+  newTab?: boolean;
 };
 
 const PrimaryButton = ({
   to,
   children,
   className = "",
+  newTab = false,
 }: PrimaryButtonProps) => {
   return (
     <NavLink
       to={to}
+      target={newTab ? "_blank" : undefined}
+      rel={newTab ? "noopener noreferrer" : undefined}
       className={`flex items-center justify-center gap-2 text-sm sm:text-base px-5 py-2.5 md:px-7 md:py-3.5 bg-primary-colour hover:bg-brand-500 hover:scale-95 text-white font-semibold rounded-full transition ${className}`}
     >
       {children}
